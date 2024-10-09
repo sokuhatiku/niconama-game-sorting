93c88faa9741b194b062e1752f81b022402a987b
Sokuhatiku <sokuhatiku@gmail.com>
2024-10-10 01:44:20 +0900

    eslintのコンフィグファイル自体をlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,40 @@
-import stylistic from "@stylistic/eslint-plugin";
-import typescript from "@typescript-eslint/eslint-plugin";
-import tsparser from "@typescript-eslint/parser";
+import stylistic from "@stylistic/eslint-plugin"
+import typescript from "@typescript-eslint/eslint-plugin"
+import tsparser from "@typescript-eslint/parser"
 
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    ignores: ["src/_bootstrap.ts", "src/parameterObject.ts"],
-    languageOptions: {
-      parser: tsparser,
-      sourceType: "module",
-      ecmaVersion: 2015,
-    },
-    plugins: {
-      "@typescript-eslint": typescript,
-      "@stylistic": stylistic,
-    },
-    rules: {
-      "no-var": "error",
-      "prefer-const": ["warn", {
-        "destructuring": "all",
-        "ignoreReadBeforeAssign": true,
-      }],
-      "no-console": "off",
-      "no-unused-vars": ["warn", { args: "none" }],
-      "no-restricted-syntax": [
-        "error",
-        {
-          "selector": "OptionalChainingExpression",
-          "message": "Optional chaining is not supported in ES2015.",
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        ignores: ["src/_bootstrap.ts", "src/parameterObject.ts"],
+        languageOptions: {
+            parser: tsparser,
+            sourceType: "module",
+            ecmaVersion: 2015,
         },
-      ],
-      "@stylistic/quotes": ["error", "double"],
-      "@stylistic/semi": ["error", "never"],
-      "@stylistic/indent": ["error", 4],
-      "@stylistic/comma-dangle": ["error", "always-multiline"],
-      "@stylistic/eol-last": ["error", "always"],
+        plugins: {
+            "@typescript-eslint": typescript,
+            "@stylistic": stylistic,
+        },
+        rules: {
+            "no-var": "error",
+            "prefer-const": ["warn", {
+                "destructuring": "all",
+                "ignoreReadBeforeAssign": true,
+            }],
+            "no-console": "off",
+            "no-unused-vars": ["warn", { args: "none" }],
+            "no-restricted-syntax": [
+                "error",
+                {
+                    "selector": "OptionalChainingExpression",
+                    "message": "Optional chaining is not supported in ES2015.",
+                },
+            ],
+            "@stylistic/quotes": ["error", "double"],
+            "@stylistic/semi": ["error", "never"],
+            "@stylistic/indent": ["error", 4],
+            "@stylistic/comma-dangle": ["error", "always-multiline"],
+            "@stylistic/eol-last": ["error", "always"],
+        },
     },
-  },
 ]
