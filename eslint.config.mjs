6ff4aeae49fe10ed0895c0c997b54afddd9768c2
Sokuhatiku <sokuhatiku@gmail.com>
2024-10-10 00:50:14 +0900

    とりあえずlinterが動く状態にした

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,34 @@
 import stylistic from "@stylistic/eslint-plugin";
+import typescript from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
 
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
-    ignores: ["src/_bootstrap.ts"],
+    ignores: ["src/_bootstrap.ts", "src/parameterObject.ts"],
     languageOptions: {
+      parser: tsparser,
       sourceType: "module",
       ecmaVersion: 2015,
     },
     plugins: {
+      "@typescript-eslint": typescript,
       "@stylistic": stylistic,
     },
     rules: {
       "no-var": "error",
       "prefer-const": "error",
       "no-console": "off",
+      "no-unused-vars": ["warn"],
+      "no-restricted-syntax": [
+        "error",
+        {
+          "selector": "OptionalChainingExpression",
+          "message": "Optional chaining is not supported in ES2015.",
+        },
+      ],
       "@stylistic/quotes": ["error", "double"],
-      "@stylistic/semi": ["error", "always"],
+      "@stylistic/semi": ["error", "never"],
       "@stylistic/indent": ["error", 2],
       "@stylistic/comma-dangle": ["error", "always-multiline"],
     },
