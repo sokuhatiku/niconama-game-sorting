e23282703b653aee607c39827d798151d15db74c
Sokuhatiku <sokuhatiku@gmail.com>
2024-10-13 00:41:55 +0900

    linter周りをまた見直し

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,44 +1,20 @@
-import stylistic from "@stylistic/eslint-plugin"
-import typescript from "@typescript-eslint/eslint-plugin"
-import tsparser from "@typescript-eslint/parser"
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    ...tseslint.configs.strictTypeChecked,
+    ...tseslint.configs.stylisticTypeChecked,
     {
-        files: ["**/*.{js,mjs,cjs,ts}"],
-        ignores: ["src/_bootstrap.ts", "src/parameterObject.ts"],
         languageOptions: {
-            parser: tsparser,
-            sourceType: "module",
-            ecmaVersion: 2015,
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            }
         },
-        plugins: {
-            "@typescript-eslint": typescript,
-            "@stylistic": stylistic,
-        },
-        rules: {
-            "no-var": "error",
-            "prefer-const": ["warn", {
-                "destructuring": "all",
-                "ignoreReadBeforeAssign": true,
-            }],
-            "no-console": "off",
-            "no-unused-vars": ["warn", { args: "none" }],
-            "no-restricted-syntax": [
-                "error",
-                {
-                    "selector": "OptionalChainingExpression",
-                    "message": "Optional chaining is not supported in ES2015.",
-                },
-            ],
-            "@typescript-eslint/explicit-function-return-type": ["error"],
-            "@stylistic/quotes": ["error", "double"],
-            "@stylistic/semi": ["error", "never"],
-            "@stylistic/indent": ["error", 4],
-            "@stylistic/comma-dangle": ["error", "always-multiline"],
-            "@stylistic/eol-last": ["error", "always"],
-            "@stylistic/comma-spacing": ["error", { "before": false, "after": true }],
-            "@stylistic/key-spacing": ["error", { "beforeColon": false, "afterColon": true }],
-            "@stylistic/type-annotation-spacing": ["error", { "before": false, "after": true, "overrides": { "arrow": { "before": true, "after": true } } }],
-        },
-    },
-]
+        linterOptions: {
+            reportUnusedDisableDirectives: false,
+        }
+    }
+);
