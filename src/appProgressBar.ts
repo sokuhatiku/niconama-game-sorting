d29ebf10c154d415dfce9763e01af362b8dd0a62
Sokuhatiku <sokuhatiku@gmail.com>
2024-10-26 19:58:05 +0900

    プログレスバーを左から右に伸びる方式から中心に向かって消えていく方式に変更

--- src/appProgressBar.ts
+++ src/appProgressBar.ts
@@ -11,16 +11,18 @@ export class AppProgressBar {
         this._rect = new g.FilledRect({
             scene: this._scene,
             cssColor: "red",
-            x: 0,
-            y: g.game.height - height,
+            x: g.game.width / 2,
+            y: g.game.height,
             width: g.game.width,
             height: height,
+            anchorX: 0.5,
+            anchorY: 1,
         });
         scene.append(this._rect);
     }
 
     public setProgress(progress: number): void {
-        this._rect.width = g.game.width * Math.max(0, Math.min(1, progress));
+        this._rect.width = g.game.width * Math.max(0, Math.min(1, 1 - progress));
         this._rect.modified();
     }
 }
